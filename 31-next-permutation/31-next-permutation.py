023c3407bcf92636c42d5fbf0f6ebb25421d1a22
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-01-22 12:22:10 +0530

    Time: 60 ms (30.88%), Space: 14.4 MB (20.13%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.py
@@ -0,0 +1,34 @@
+class Solution:
+    
+    def rev(self, nums, ind):
+        x=ind
+        j=len(nums)-1
+        while(x<j):
+            nums[x],nums[j]= nums[j], nums[x]
+            x+=1
+            j-=1
+        
+            
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n= len(nums)
+        i=n-2
+        # for i in range(n-2,-1,-1):
+        while i>=0:
+            if nums[i]<nums[i+1]:
+                break;
+            i-=1
+    
+        if i<0:
+            nums.reverse()
+            return
+        for j in range(len(nums)-1,i,-1):
+            if nums[j]>nums[i]:
+                # self.swap(nums[j],nums[i])
+                nums[i],nums[j]= nums[j], nums[i]
+                break
+        self.rev(nums, i+1)
+            
+        
