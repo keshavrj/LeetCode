acef886d95fe3dcd9c250b28065a3ef82c546f64
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-02-22 13:53:57 +0530

    Time: 8 ms (93.21%), Space: 21.7 MB (59.07%) - LeetHub

--- 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.cpp
@@ -11,29 +11,44 @@
  */
 class Solution {
 public:
-    int h, ans;
-    bool flag=0;
-    int height(TreeNode* root){
-        if(!root)return 0;
-        return 1+ max(height(root->left), height(root->right));
-    }
-    void solve(TreeNode* root, int h)
+    int ans;
+    // int h, ans;
+    // bool flag=0;
+    // int height(TreeNode* root){
+    //     if(!root)return 0;
+    //     return 1+ max(height(root->left), height(root->right));
+    // }
+    // void solve(TreeNode* root, int h)
+    // {
+    //     if(!root)return;
+    //     if(h==1){
+    //         flag=1;
+    //         ans= root->val;
+    //         return;
+    //     }
+    //     solve(root->left, h-1);
+    //     if(!flag)
+    //     solve(root->right, h-1);
+    //     // return root->val;
+    // }
+    void solve(TreeNode* root, int Depth, int &height)
     {
         if(!root)return;
-        if(h==1){
-            flag=1;
+        if(Depth>height)
+        {
             ans= root->val;
-            return;
+            height= Depth;
         }
-        solve(root->left, h-1);
-        if(!flag)
-        solve(root->right, h-1);
-        // return root->val;
+        solve(root->left, Depth+1, height);
+        solve(root->right, Depth+1, height);
     }
     int findBottomLeftValue(TreeNode* root) {
-        h=height(root);
-        // cout<<h;
-        solve(root, h);
+        // h=height(root);
+        // // cout<<h;
+        // solve(root, h);
+        // return ans;
+        int h= 0;
+        solve(root, 1, h);
         return ans;
     }
 };
