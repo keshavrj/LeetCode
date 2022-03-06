37febd0fab047ada8c9dbb69d9e31dadae5b7dd0
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-03-06 06:20:29 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.86%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
-    int countOrders(int n) {
-        int mod= 1e9+7;
-        long res=1;
-        for(int i=1;i<=n;i++)
-        {
-            res= (res* (2*i-1)*i)%mod; // (2n-1)*2n ->total purmutation for nth value, delivery will come always after pickup so divide by 2; 
-        }
-        return (int)res%mod;
+    int countOrders(int n, long val=1) {
+        if(n==0)return (int)val%1000000007;
+        return countOrders(n-1,(n*(2*n-1)*val)%1000000007 );
     }
 };
