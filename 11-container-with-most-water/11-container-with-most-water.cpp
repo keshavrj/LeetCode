9c042c71872d456b2e4e3b08a7f1451f3bad5c91
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-03-08 08:04:02 +0530

    Time: 72 ms (98.34%), Space: 59 MB (81.70%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -4,10 +4,11 @@ public:
         int l=0, r= h.size()-1, area=0;
         while(l<r)
         {
-            area= max(area, (r-l)*(min(h[r], h[l])));
-            if(h[r]<h[l])r--;
-            else if(h[r]>h[l])l++;
-            else l++, r--;
+            int H= min(h[r], h[l]);
+            area= max(area, (r-l)*H);
+            while(l<r && h[l]<=H)l++;
+            while(l<r && h[r]<=H)r--;
+            
         }
         return area;
     }
