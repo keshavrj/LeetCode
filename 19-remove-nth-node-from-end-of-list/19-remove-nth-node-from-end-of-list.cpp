c4ff75a19e6ade51aae9fb920a587f9dceb59b7d
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-03-08 08:26:42 +0530

    Time: 3 ms (88.99%), Space: 10.8 MB (33.57%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -11,27 +11,40 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int len=0;
-        auto p= head;
-        while(p)len++, p=p->next;
-        n= len-n+1;
-        if(n==1)return head->next;
-        p= head;
-        int cnt=1;
-        while(p && cnt<n)
+        
+        ListNode *start= new ListNode(0, head);
+        ListNode *slow= start, *fast= start;
+        for(int i=1;i<=n+1;i++)
+            fast= fast->next;
+        while(fast)
         {
-            if(cnt==n-1)
-            {
-                // auto q= p->next;
-                p->next=p->next->next;
-                // q->next=NULL;
-                // delete(q);
-                return head;
-            }
-            cnt++;
-            p=p->next;
-            
+            fast= fast->next;
+            slow= slow->next;
         }
-        return NULL;
+        slow->next=slow->next->next;
+        return start->next;
+        
+//         int len=0;
+//         auto p= head;
+//         while(p)len++, p=p->next;
+//         n= len-n+1;
+//         if(n==1)return head->next;
+//         p= head;
+//         int cnt=1;
+//         while(p && cnt<n)
+//         {
+//             if(cnt==n-1)
+//             {
+//                 // auto q= p->next;
+//                 p->next=p->next->next;
+//                 // q->next=NULL;
+//                 // delete(q);
+//                 return head;
+//             }
+//             cnt++;
+//             p=p->next;
+            
+//         }
+//         return NULL;
     }
 };
