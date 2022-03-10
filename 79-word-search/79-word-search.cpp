401ff8947a1d4eabe62a18f73df1102d8ae3fd18
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-03-10 11:58:01 +0530

    Time: 334 ms (75.93%), Space: 8 MB (75.31%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -15,7 +15,7 @@ public:
         if(i<0 or j<0 or i>=board.size() or j>=board[0].size() or board[i][j]!=word[k])
             return false;
         char c= board[i][j];
-        board[i][j]= '!';
+        board[i][j]= '!'; // marking this as visited;
         if(dfs(board, i+1, j, word, k+1)||dfs(board, i-1, j, word, k+1)||
           dfs(board, i, j+1, word, k+1)||dfs(board, i, j-1, word, k+1))return true;
         
