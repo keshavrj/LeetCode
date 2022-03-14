9db2c3da90abf63dea97de271031c6d7e27d76e7
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-03-14 08:56:02 +0530

    Time: 0 ms (100.00%), Space: 6 MB (86.08%) - LeetHub

--- 796-rotate-string/796-rotate-string.cpp
+++ 796-rotate-string/796-rotate-string.cpp
@@ -3,14 +3,15 @@ public:
     bool rotateString(string s, string goal) {
         if(s.size()!=goal.size())return 0;
         goal+=goal;
-        int i,j, k=0;
-        for( i=0, j=0; i<s.size();i++)
-        {
-            for(j=0;j<s.size();j++)
-                if(goal[i+j]!=s[j])break;
-            if(j==s.size())return true;
-        }
+        return goal.find(s)!=string::npos;
+        // int i,j, k=0;
+//         for( i=0, j=0; i<s.size();i++)
+//         {
+//             for(j=0;j<s.size();j++)
+//                 if(goal[i+j]!=s[j])break;
+//             if(j==s.size())return true;
+//         }
         
-        return false;
+//         return false;
     }
 };
