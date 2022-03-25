72eba9fc5f21473f5fb1533df447fa5cac57a90c
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-03-25 11:33:37 +0530

    Added solution - LeetHub

--- Smallest greater elements in whole array - GFG/smallest-greater-elements-in-whole-array.cpp
+++ Smallest greater elements in whole array - GFG/smallest-greater-elements-in-whole-array.cpp
@@ -31,18 +31,33 @@ int main()
 }
 // } Driver Code Ends
 
-
+int find_Next(vector<int> &arr, int k)
+{
+    if(k==arr.back())return -1;
+ int l=0, r=arr.size()-1;
+ while(l<=r)
+ {
+     int mid= l+ (r-l)/2;
+     if(arr[mid]<=k)
+        l= mid+1;
+     else r= mid-1;
+ }
+ return l;
+    
+}
 int* greaterElement(int arr[], int n)
 {
     int nl= -1e7;
-   multiset<int> st;
-    for(int i=0;i<n;i++)st.insert(arr[i]);
+    vector<int> st;
+    for(int i=0;i<n;i++)st.push_back(arr[i]);
+    sort(st.begin(), st.end());
     for(int i=0;i<n;i++)
     {
-        auto x= st.upper_bound(arr[i]);
-        if(x==st.end())
+        // auto x= st.upper_bound(arr[i]);
+        int x= find_Next(st, arr[i]);
+        if(x==-1)
             arr[i]= nl;
-        else arr[i]= *x;
+        else arr[i]= st[x];
     }
     return arr;
    
