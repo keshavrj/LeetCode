8314ad7c5ce7e4e74ac5f886119c81bc34600bda
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-03-26 20:36:05 +0530

    Added solution - LeetHub

---
+++ Maximum average subarray - GFG/maximum-average-subarray.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int findMaxAverage(int arr[], int n, int k) {
+        // code here
+        int sm=accumulate(arr, arr+k, 0);
+        int max_sum=sm;
+        int index=0;
+        for(int i=1,j=k;j<n;i++, j++)
+        {
+            sm=sm-arr[i-1]+arr[j];
+            if(sm>max_sum)
+            index=i, sm= max_sum;
+        }
+        return index;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> k;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMaxAverage(arr, n, k);
+        for (int i = ans; i < ans + k; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
