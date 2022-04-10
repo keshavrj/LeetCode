cc52d6a9d447b468bbeb6b2014cc2c352f5dad3b
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-04-10 09:48:14 +0530

    Time: 7 ms (43.28%), Space: 8.4 MB (69.92%) - LeetHub

--- 682-baseball-game/682-baseball-game.cpp
+++ 682-baseball-game/682-baseball-game.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int calPoints(vector<string>& ops) {
-        vector<int> a, v;
+        vector<int>  v;
         for(auto i:ops)
         {
             if(i=="C")
                 v.pop_back();
             else if(i=="D")
             {
-                if(!v.empty())v.push_back(v.back()*2);
-                else 
-                    v.push_back(a.back()*2);
+                // if(!v.empty())v.push_back(v.back()*2);
+                // else 
+                //     v.push_back(a.back()*2);
+                v.push_back(v.back()*2);
             }
             else if(i=="+"){
                 int sz= v.size();
@@ -18,7 +19,7 @@ public:
             }
             else{
                 v.push_back(stoi(i));
-                a.push_back(v.back());
+                // a.push_back(v.back());
             }
             
         }
