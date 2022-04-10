ff36155f0f7be712453a39e7e541e7946e5b049a
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-04-10 09:49:02 +0530

    Time: 6 ms (46.67%), Space: 8.3 MB (69.92%) - LeetHub

--- 682-baseball-game/682-baseball-game.cpp
+++ 682-baseball-game/682-baseball-game.cpp
@@ -7,20 +7,12 @@ public:
             if(i=="C")
                 v.pop_back();
             else if(i=="D")
-            {
-                // if(!v.empty())v.push_back(v.back()*2);
-                // else 
-                //     v.push_back(a.back()*2);
                 v.push_back(v.back()*2);
-            }
             else if(i=="+"){
                 int sz= v.size();
                 v.push_back(v[sz-1]+v[sz-2]);
             }
-            else{
-                v.push_back(stoi(i));
-                // a.push_back(v.back());
-            }
+            else v.push_back(stoi(i));
             
         }
         return accumulate(v.begin(), v.end(), 0);
