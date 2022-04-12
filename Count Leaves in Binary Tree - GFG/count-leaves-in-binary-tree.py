36415b076f5e65ed029bbc59d8d5fc952ebe0deb
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-04-12 21:35:26 +0530

    Added solution - LeetHub

--- Count Leaves in Binary Tree - GFG/count-leaves-in-binary-tree.py
+++ Count Leaves in Binary Tree - GFG/count-leaves-in-binary-tree.py
@@ -14,7 +14,7 @@ def countLeaves(root):
     # Code here
     if not root:
         return 0
-    if root.left==None and root.right==None:
+    if not root.left and not root.right:
         return 1
     return countLeaves(root.left)+countLeaves(root.right)
 
