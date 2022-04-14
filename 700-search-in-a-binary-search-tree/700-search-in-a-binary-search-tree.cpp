b3713a20f649ccba587b5941f52165119c00b532
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-04-14 11:20:39 +0530

    Time: 49 ms (66.82%), Space: 34.9 MB (24.22%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -16,6 +16,6 @@ public:
         if(root->val==val)return root;
          if(root->val>val)
              return searchBST(root->left,val);
-        else return searchBST(root->right, val);
+        return searchBST(root->right, val);
     }
 };
