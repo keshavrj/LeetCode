64d775cdec2d37b66e8b6675af1163f6edceb0f8
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-04-21 11:33:27 +0530

    Time: 157 ms (45.66%), Space: 42.1 MB (52.05%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,20 +1,44 @@
 class MyHashSet {
 public:
-    vector<bool> hash;
+    // vector<bool> hash;
+    vector<list<int>> hash;
+    int prime;
+    int pos(int key)
+    {
+        return key%prime;
+    }
+    list<int>::iterator search_list(int key)
+    {
+        int h = pos(key);
+        return find(hash[h].begin(), hash[h].end(), key);
+    }
+    
     MyHashSet() {
-        hash.assign((int)1e6+2, false);
+        prime=1007;
+        // hash.assign(prime);
+        hash.resize(prime);
     }
     
     void add(int key) {
-        hash[key]=true;
+        // hash[key]=true;
+        int h= pos(key);
+        auto it= search_list(key);
+        if(it==hash[h].end())
+            hash[h].push_back(key);
     }
     
     void remove(int key) {
-        hash[key]=false;
+        // hash[key]=false;
+        int h= pos(key);
+        auto it= search_list(key);
+        if(it!=hash[h].end())
+            hash[h].erase(it);
     }
     
     bool contains(int key) {
-        return hash[key];
+        // return hash[key];
+        int h= pos(key);
+        return search_list(key)!=hash[h].end();
     }
 };
 
