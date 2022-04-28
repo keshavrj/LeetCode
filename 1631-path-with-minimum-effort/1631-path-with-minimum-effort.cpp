8c6c805ba4b0fc4eb1eaa1c08acddbd6951ce521
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-04-28 17:59:09 +0530

    Time: 439 ms (36.94%), Space: 33.9 MB (47.84%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -3,14 +3,14 @@ public:
     int minimumEffortPath(vector<vector<int>>& h) {
         int n = h.size(), m= h[0].size(); 
         vector<vector<int>> cost(n,vector<int> (m,INT_MAX));
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
-        pq.push({0,0}); //i,j;
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>> > pq;
+        pq.push({0,0,0}); //effort,i,j; effort is to maintain heap 
         cost[0][0]= 0;
         //Dijkstra
         int dir[5]= { -1, 0, 1, 0, -1};
         while(!pq.empty())
         {
-            int i=pq.top().first, j= pq.top().second;
+            int i=pq.top()[1], j= pq.top()[2];
             pq.pop();
             if(i==n-1 && j==m-1)
                 return cost[n-1][m-1];
@@ -24,7 +24,7 @@ public:
                         if(new_eff<cost[X][Y])
                         {
                             cost[X][Y]= new_eff;
-                            pq.push({X,Y});
+                            pq.push({new_eff,X,Y});
                         }
                 }
             }
