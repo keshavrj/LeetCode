f564fd67d705689fbba89249e1458845395c705d
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-05-01 12:55:16 +0530

    Time: 298 ms (47.40%), Space: 31.8 MB (42.65%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,53 +1,33 @@
 class Solution {
 public:
-   int a[4][2]={{0,-1},{-1,0},{1,0},{0,1}};
-
-int minimumEffortPath(vector<vector<int>>& hts) {
-    
-    // multiset<pair<int,int>>ms;
-     queue<pair<int,int>>ms;
-    
-    int n=hts.size();
-    int m=hts[0].size();
-    vector<vector<int>>eff(n,vector<int>(m,INT_MAX));
-    
-    // ms.insert({0,0});
-     ms.push({0,0});
-    eff[0][0]=0;
-    
-    while(ms.size()>0)
-    {
-        // pair<int,int>p=*ms.begin();
-         pair<int,int>p=ms.front();
-        
-       
-        int x=p.first;
-        int y=p.second;
-        
-        // ms.erase(ms.begin());
-        ms.pop();
-
-        
-        for(int i=0;i<4;i++)
+    int minimumEffortPath(vector<vector<int>>& h) {
+        int n = h.size(), m= h[0].size(); 
+        vector<vector<int>> cost(n,vector<int> (m,INT_MAX));
+        queue<pair<int,int> > pq;
+        pq.push({0,0}); //i,j;
+        cost[0][0]= 0;
+        //Dijkstra
+        int dir[5]= { -1, 0, 1, 0, -1};
+        while(!pq.empty())
         {
-            int nx=x+a[i][0];
-            int ny=y+a[i][1];
-            
-            if(nx>=0 && ny>=0 && nx<n && ny<m)
+            int i=pq.front().first, j= pq.front().second;
+            pq.pop();
+            for(int d=0;d<4;d++)
             {
-                int newEff=max(abs(hts[x][y]-hts[nx][ny]),eff[x][y]);
-                
-                if(newEff<eff[nx][ny])
+                int X = i+dir[d], Y= j+dir[d+1];
+                if(X>=0 && X<n && Y>=0 && Y<m)
                 {
-                    eff[nx][ny]=newEff;
-                    // ms.insert({nx,ny});
-                    ms.push({nx,ny});
+                    int new_eff= max(cost[i][j], abs(h[i][j]-h[X][Y]));
+                        if(new_eff<cost[X][Y])
+                        {
+                            cost[X][Y]= new_eff;
+                            pq.push({X,Y});
+                        }
                 }
             }
+                
+            
         }
+        return cost[n-1][m-1];
     }
-    
-    return eff[n-1][m-1];
-    
-}
 };
