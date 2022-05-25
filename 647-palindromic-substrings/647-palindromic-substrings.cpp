3d172a826c3b731a1ccaa613db59997db00fc04b
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-05-25 20:08:41 +0530

    Time: 30 ms (36.99%), Space: 20.2 MB (13.54%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int n=s.size(), cnt=0;
+        int n=s.size();
+        int cnt=n;
         vector<vector<int>> dp(n+1, vector<int>(n+1,0));
         for(int i=0;i<=n;i++){
             dp[0][i]=dp[1][i]=1;// length 0 and ending at i;
@@ -10,16 +11,10 @@ public:
         {
             for(int j= i;j<=n;j++){
                 if(s[j-1]==s[j-i] and dp[i-2][j-1]==1) // length i-2 and ending at j-1;
-                    dp[i][j]=1;
+                { dp[i][j]=1;
+                  cnt++;
+                 }
             }
-        }
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=n;j++)
-            {        if(dp[i][j])cnt++;
-                // cout<<dp[i][j]<<" ";
-            }
-            // cout<<endl;
         }
         return cnt;
     }
