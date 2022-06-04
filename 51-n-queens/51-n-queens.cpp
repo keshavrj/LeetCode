b4b3d7aeda0b3433a7207193523d980071b090e6
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-06-04 15:02:18 +0530

    Time: 3 ms (96.12%), Space: 7.2 MB (86.66%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    vector<int> leftRow, lowerD, upperD;
     bool isValid(int row, int col, vector<string> &q){
         //checking cols;
         for(int i=col;i>=0;i--)
@@ -22,16 +23,26 @@ public:
         }
         for(int row=0;row<n;row++)
         {
-            if(isValid(row,col,q)){
+            if(leftRow[row]==0 && lowerD[col+row]==0 && upperD[n-1+ col-row]==0){
                 q[row][col]='Q';
+                leftRow[row]=1;
+                lowerD[col+row]=1; 
+                upperD[n-1+ col-row]=1;
                 solve(ans,q,col+1,n);
                  q[row][col]='.';
+                leftRow[row]=0;
+                lowerD[col+row]=0; 
+                upperD[n-1+ col-row]=0;
+                
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<string> q(n,string(n,'.'));
         vector<vector<string>> ans;
+        leftRow.resize(n,0);
+        lowerD.resize(2*n-1,0);
+        upperD.resize(2*n-1,0);
         solve(ans,q,0,n);
         return ans;
     }
