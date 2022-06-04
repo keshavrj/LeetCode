0e992a5cc124f033f9100dd5a8c3d87ecda27d4e
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-06-04 14:34:51 +0530

    Time: 7 ms (75.47%), Space: 7.3 MB (67.79%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isValid(int row, int col, vector<string> &q){
+        //checking cols;
+        for(int i=row;i>=0;i--)
+            if(q[i][col] == 'Q') return false;
+        
+        // check left diagonal
+        for(int i=row,j=col;i>=0&&j>=0;i--,j--)
+            if(q[i][j] == 'Q') return false;
+        
+        //check right diagonal
+        for(int i=row,j=col;i>=0 && j<q.size();--i,++j)
+            if(q[i][j] == 'Q') return false;
+        
+        return true;
+    }
+    void solve(vector<vector<string>> &ans, vector<string> &q, int row, int n){
+        if(row==n){
+            ans.push_back(q);
+            return;
+        }
+        for(int col=0;col<n;col++)
+        {
+            if(isValid(row,col,q)){
+                q[row][col]='Q';
+                solve(ans,q,row+1,n);
+                 q[row][col]='.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> q(n,string(n,'.'));
+        vector<vector<string>> ans;
+        solve(ans,q,0,n);
+        return ans;
+    }
+};
