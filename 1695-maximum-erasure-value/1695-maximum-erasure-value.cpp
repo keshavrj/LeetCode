dfb15d91dff6406c286e67bd65d8f1b8223633de
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-06-12 12:17:50 +0530

    Time: 505 ms (49.46%), Space: 99.5 MB (89.43%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -3,10 +3,7 @@ public:
     int maximumUniqueSubarray(vector<int>& nums) {
         unordered_map<int,int> mp;
         int sm=0,l=0, r=0, n= nums.size(), ans=0;
-        // vector<int> dp(n,0);
-        // dp[0]= nums[0];
-        // for(int i=1;i<n;i++)
-        //     dp[i]= dp[i-1]+nums[i];
+  
         while(r<n){
             mp[nums[r]]++;
             sm+=nums[r];
@@ -16,7 +13,6 @@ public:
                 sm-=nums[l];
                 l++;
             }
-                // sm= l>0 ? dp[r]-dp[l-1] : dp[r];
             ans= max(ans, sm);
             r++;
         }
