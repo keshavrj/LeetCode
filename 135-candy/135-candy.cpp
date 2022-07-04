0a7f4047cf585fb42aebfca91aa780458476c29d
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-07-04 19:53:52 +0530

    Time: 34 ms (49.44%), Space: 17.8 MB (40.68%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -3,13 +3,16 @@ public:
     int candy(vector<int>& arr)
     {
         int n= arr.size();
+        int ans=0;
         vector<int> dp(n,1);
         for(int i=1;i<n;i++){
             if(arr[i]>arr[i-1])dp[i]= dp[i-1]+1;
         }
+        ans=dp[n-1];
         for(int i=n-2;i>-1;i--){
             if(arr[i]>arr[i+1])dp[i]= max(dp[i], dp[i+1]+1);
+            ans+=dp[i];
         }
-        return accumulate(dp.begin(), dp.end(),0);
+        return ans;
     }   
 };
