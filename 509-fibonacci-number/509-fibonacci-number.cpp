29636fdef48fada78029b7f8d88c03417bad8a1d
Keshav Raj <48690550+keshavrj@users.noreply.github.com>
2022-07-06 15:52:32 +0530

    Time: 17 ms (28.08%), Space: 6 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
-        int arr[n+4];
-        arr[0]=0;
-        arr[1]=1;
-        for(int i=2;i<=n;i++)
-            arr[i]= arr[i-1]+arr[i-2];
-        
-        return arr[n];
+        if(n<=1)return n;
+        return fib(n-1)+fib(n-2);
     }
 };
